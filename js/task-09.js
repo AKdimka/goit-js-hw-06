b13d45ecd6f00107c6f6192d59d6651dac700957
AKdimka <88268598+AKdimka@users.noreply.github.com>
2021-12-05 13:51:50 +0200

    finish hw

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,15 @@
+const refs = {
+	changeColorBtn: document.querySelector('.change-color'),
+	changeColorLabel: document.querySelector('.color')
+}
+
+const onChangeColorBtnClick = () => {
+	const color = getRandomHexColor()
+	document.body.style.background = color;
+	refs.changeColorLabel.textContent = color;
+}
+refs.changeColorBtn.addEventListener('click', onChangeColorBtnClick);
+
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+	return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
