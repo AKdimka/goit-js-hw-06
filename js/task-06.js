3ed3aa5562969c7180f0f8f058400ac666ccfbf5
Dmytro Bryhadyr <88268598+AKdimka@users.noreply.github.com>
2023-06-24 22:49:47 +0300

    practice again 6

--- js/task-06.js
+++ js/task-06.js
@@ -1,4 +1,62 @@
-const inputField = document.querySelector('#validation-input');
+const input = document.querySelector('#validation-input');
+const validLength = input.getAttribute('data-length')
+
+const inputBlur = (event) => {
+	input.classList.add('valid')
+
+	event.currentTarget.value.length === Number(validLength) ?
+		input.classList.replace('invalid', 'valid') :
+		input.classList.replace('valid', 'invalid')
+}
+
+input.addEventListener('blur', inputBlur)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* const inputField = document.querySelector('#validation-input');
 const dataLength = inputField.getAttribute('data-length');
 
 inputField.addEventListener('blur', onBlur);
@@ -9,4 +67,4 @@ function onBlur() {
 	inputField.value.length === Number(dataLength) ?
 		inputField.classList.replace('invalid', 'valid') :
 		inputField.classList.replace('valid', 'invalid');
-};
+}; */
