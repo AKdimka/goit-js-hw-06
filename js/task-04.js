7848da33a34fae0275983a0d5d4d46df00775033
Dmytro Bryhadyr <88268598+AKdimka@users.noreply.github.com>
2023-06-24 19:45:26 +0300

    practice again 4

--- js/task-04.js
+++ js/task-04.js
@@ -1,4 +1,51 @@
-let value = 0;
+const decrementBtn = document.querySelector('[data-action="decrement"]');
+const incrementBtn = document.querySelector('[data-action="increment"]');
+const value = document.querySelector('#value');
+
+let counterValue = 0;
+
+const changeValue = (num) => {
+	counterValue += num
+	value.textContent = counterValue;
+}
+
+decrementBtn.addEventListener('click', () => changeValue(-1));
+incrementBtn.addEventListener('click', () => changeValue(+1));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* let value = 0;
 
 const refs = {
 	increment: document.querySelector('[data-action="increment"]'),
@@ -12,4 +59,4 @@ const changeValue = (step) => {
 };
 
 refs.increment.addEventListener('click', () => changeValue(+1));
-refs.decrement.addEventListener('click', () => changeValue(-1));
+refs.decrement.addEventListener('click', () => changeValue(-1)); */
