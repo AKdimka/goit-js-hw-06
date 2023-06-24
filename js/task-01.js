05041b09a3c02c4a411d4529b70b19da0d85aec1
Dmytro Bryhadyr <88268598+AKdimka@users.noreply.github.com>
2023-06-24 13:48:29 +0300

    practice again

--- js/task-01.js
+++ js/task-01.js
@@ -1,6 +1,50 @@
-const categoriesItemsCount = document.querySelectorAll(".item h2")
+const categories = document.querySelectorAll('.item > h2');
+
+//console.log(categories) /* nodeList: pseudoArrey */
+
+const categoriesCounter = categories.length;
+
+//console.log(`Number of categories: ${categoriesCounter}`);
+
+console.log('Number of categories: ' + categoriesCounter);
+
+
+categories.forEach(item => console.log(`Category: ${item.textContent}
+Elements:	${item.nextElementSibling.children.length}`))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* const categoriesItemsCount = document.querySelectorAll(".item h2")
 console.log(`Number of categories: ${categoriesItemsCount.length}`)
 
 categoriesItemsCount.forEach(ulCategories =>
 	console.log(`Category: ${ulCategories.textContent}
-Elements: ${ulCategories.nextElementSibling.children.length}`))
+Elements: ${ulCategories.nextElementSibling.children.length}`)) */
