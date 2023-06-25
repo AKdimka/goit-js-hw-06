4d070c6232cc05695def810dadc46f811a207ef6
Dmytro Bryhadyr <88268598+AKdimka@users.noreply.github.com>
2023-06-25 19:43:23 +0300

    practice again 10

--- js/task-10.js
+++ js/task-10.js
@@ -1,4 +1,53 @@
-const refs = {
+const input = document.querySelector('input');
+const createBtn = document.querySelector('[data-create]');
+const destroyBtn = document.querySelector('[data-destroy]');
+const boxesContainer = document.querySelector('#boxes');
+
+let inputValue = 0;
+
+createBtn.addEventListener('click', onCreateClick);
+destroyBtn.addEventListener('click', onDestroyClick);
+
+function getRandomHexColor() {
+	return `#${Math.floor(Math.random() * 16777215)
+		.toString(16)
+		.padStart(6, 0)}`;
+}
+
+function onCreateClick() {
+	inputValue = Number(input.value);
+	const size = 20;
+	const boxes = [];
+
+	for (let i = 1; i <= inputValue; i += Number(input.step)) {
+
+		if (inputValue < Number(input.min) || inputValue > Number(input.max)) {
+			alert(`Значение должно быть в пределах ${input.min} - ${input.max}`)
+			break;
+		}
+		const stringSize = (size + 10 * i) + 'px';
+
+		boxes.push(`<div style='width:${stringSize};
+		height:${stringSize};
+		background-color:${getRandomHexColor()}'></div>`)
+	}
+	boxesContainer.innerHTML = boxes.join('');
+	input.value = '';
+}
+
+function onDestroyClick() {
+	boxesContainer.innerHTML = ''
+	input.value = ''
+}
+
+
+
+
+
+
+
+
+/* const refs = {
 	create: document.querySelector('button[data-create]'),
 	destroy: document.querySelector('button[data-destroy]'),
 	input: document.querySelector('input'),
@@ -40,4 +89,4 @@ function getRandomHexColor() {
 function destroyBoxes() {
 	refs.container.innerHTML = '';
 	elementSize = 20;
-}
+} */
