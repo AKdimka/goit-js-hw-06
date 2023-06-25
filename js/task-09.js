d00c532aaafce21868660bcdc3332d5df47ddade
Dmytro Bryhadyr <88268598+AKdimka@users.noreply.github.com>
2023-06-25 12:07:57 +0300

    practice again 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,4 +1,52 @@
-const refs = {
+const body = document.querySelector('body');
+const colorCode = document.querySelector('.color');
+const changeColorBtn = document.querySelector('button.change-color');
+
+changeColorBtn.addEventListener('click', changeBgColor)
+
+function getRandomHexColor() {
+	return `#${Math.floor(Math.random() * 16777215)
+		.toString(16)
+		.padStart(6, 0)}`;
+}
+
+function changeBgColor() {
+	const hexCode = getRandomHexColor();
+
+	colorCode.textContent = hexCode;
+
+	body.style.backgroundColor = hexCode;
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* const refs = {
 	changeColorBtn: document.querySelector('.change-color'),
 	changeColorLabel: document.querySelector('.color')
 }
@@ -12,4 +60,4 @@ refs.changeColorBtn.addEventListener('click', onChangeColorBtnClick);
 
 function getRandomHexColor() {
 	return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-}
+} */
